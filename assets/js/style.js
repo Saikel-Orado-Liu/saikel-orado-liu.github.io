b46f8bfb5c758eab170607db981c55f86a5fc4ff
GameGeek-Saikel <saikel-orado@foxmail.com>
2024-02-18 02:08:40 +0800

    update new svg

--- assets/js/style.js
+++ assets/js/style.js
@@ -3,45 +3,41 @@ var styleImg = document.getElementById("style-img");
 var styleText = document.getElementById("style-text");
 
 if (window.matchMedia && window.matchMedia('(prefers-color-scheme: light)').matches) {
-	styleImg.src = "/assets/img/sun.svg";
+	styleImg.classList.remove("bx-moon");
+	styleImg.classList.add("bx-sun");
 	styleText.setAttribute("id", "light-style");
-	styleText = document.getElementById("light-style");
 } else {
-	styleImg.src = "/assets/img/moon.svg";
+	styleImg.classList.remove("bx-sun");
+	styleImg.classList.add("bx-moon");
 	styleText.setAttribute("id", "dark-style");
-	styleText = document.getElementById("dark-style");
 }
 
 style.onclick = function() {
 	if (window.matchMedia && window.matchMedia('(prefers-color-scheme: light)').matches) {
 		if(document.body.classList.contains("light-style")) document.body.classList.toggle("light-style");
 		document.body.classList.toggle("dark-style");
-		if (document.body.classList.contains("dark-style")) {
-			styleImg.src = "/assets/img/moon.svg";
-			styleText.setAttribute("id", "dark-style");
-			styleText = document.getElementById("dark-style");
-			updateText("dark-style");
-		} else {
-			styleImg.src = "/assets/img/sun.svg";
-			styleText.setAttribute("id", "light-style");
-			styleText = document.getElementById("light-style");
-			updateText("light-style");
-		}
+		if (document.body.classList.contains("dark-style")) toggleDark(); else toggleLight();
 	} else {
 		if(document.body.classList.contains("dark-style")) document.body.classList.toggle("dark-style");
 		document.body.classList.toggle("light-style");
-		if (document.body.classList.contains("light-style")) {
-			styleImg.src = "/assets/img/sun.svg";
-			styleText.setAttribute("id", "light-style");
-			styleText = document.getElementById("light-style");
-			updateText("light-style");
-		} else {
-			styleImg.src = "/assets/img/moon.svg";
-			styleText.setAttribute("id", "dark-style");
-			styleText = document.getElementById("dark-style");
-			updateText("dark-style");
-		}
+		if (document.body.classList.contains("light-style")) toggleLight(); else toggleDark();
 	}
+}
+
+function toggleDark() {
+	styleImg.classList.remove("bx-sun");
+	styleImg.classList.add("bx-moon");
+	styleText.setAttribute("id", "dark-style");
+	styleText = document.getElementById("dark-style");
+	updateText("dark-style");
+}
+
+function toggleLight() {
+	styleImg.classList.remove("bx-moon");
+	styleImg.classList.add("bx-sun");
+	styleText.setAttribute("id", "light-style");
+	styleText = document.getElementById("light-style");
+	updateText("light-style");
 }
 
 async function updateText(id) {
